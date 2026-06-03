63480e7914eda49546cf6d454082774c8512d199
tomaszJablonski <tjablonski1990@gmail.com>
2026-06-03 21:21:26 +0200

    appium builder

--- src/test/java/training/TestAppiumBasic.java
+++ src/test/java/training/TestAppiumBasic.java
@@ -2,11 +2,15 @@ package training;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Map;
 
 public class TestAppiumBasic {
 
@@ -18,13 +22,29 @@ public class TestAppiumBasic {
     public void test() throws URISyntaxException, MalformedURLException {
         //Appium code -> appium Server -> Mobile
         //new AndroidDriver(AppiumServer,capabilities)
+
+        AppiumDriverLocalService service =
+                new AppiumServiceBuilder()
+                        .withAppiumJS(new File("C:\\Users\\MrMag\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+                        .withIPAddress("127.0.0.1")
+                        .usingPort(4723)
+                        .withEnvironment(Map.of("APPIUM_HOME", "C:\\Users\\MrMag"))
+                        .build();
+
+        service.start();
+
+
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Pixel4a");
         options.setApp("D:\\1.TESTOWANIE\\3. Testowanie aplikacji na telefonie\\Kurs Udemy\\intellij\\Projekt1\\AppiumProject\\src\\test\\resources\\ApiDemos-debug.apk");
 
         AndroidDriver androidDriver = new AndroidDriver(new URI("http://127.0.0.1:4723/").toURL(), options);
         androidDriver.quit();
-        //Actual automation
 
+
+        service.stop();
+        service.close();
+
+        //Actual automation
     }
 }
