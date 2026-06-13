7e97b32a2f4e1a13993c1bed25ab8ca45c2b4972
tomaszJablonski <tjablonski1990@gmail.com>
2026-06-13 14:56:14 +0200

    new configuration

--- src/test/java/generalStoreApk/utils/GeneralStoreBase.java
+++ src/test/java/generalStoreApk/utils/GeneralStoreBase.java
@@ -1,4 +1,28 @@
 package generalStoreApk.utils;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.util.Map;
+
 public class GeneralStoreBase {
+
+    public AndroidDriver driver;
+    public AppiumDriverLocalService service;
+
+    @BeforeClass
+    public void configureAppium(){
+        service =
+                new AppiumServiceBuilder()
+                        .withAppiumJS(new File("C:\\Users\\MrMag\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+                        .withIPAddress("127.0.0.1")
+                        .usingPort(4723)
+                        .withEnvironment(Map.of("APPIUM_HOME", "C:\\Users\\MrMag"))
+                        .build();
+
+        service.start();
+    }
 }
